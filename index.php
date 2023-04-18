d64c81ddf5f6fc3dcc833ed3e058e81cabda1c5d
GiuliaSetti <giulia.setti96@gmail.com>
2023-04-18 19:02:59 +0200

    php: hotel stylized in a bootstrap table

--- index.php
+++ index.php
@@ -50,35 +50,72 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+
     <title>PHP - Hotel</title>
 </head>
 <body>
 
-    <!-- stampare in pagina l'array di hotel senza stilizzazione -->
-    <h1>HOTELS</h1>
-        <div>
-    
-            <?php 
+    <div class="container my-5">
+        <h1 class="text-center">HOTELS</h1>
 
-                foreach ($hotels as $hotel){
+        <div class="my-5">
+            <table class="table table-hover">
 
-                    foreach ($hotel as $key => $info){
+                <thead>
+                    <th scope="col">Hotel Name</th>
+                    <th scope="col">Hotel Description</th>
+                    <th scope="col">Parking Availability</th>
+                    <th scope="col">Hotel Vote</th>
+                    <th scope="col">Distance From the City Centre</th>
+                </thead>
 
-                        echo "<b>{$key}</b>: {$info} <br>";
+                <tbody>
 
-                    }
+                    <tr>
+                     
+                        <?php foreach($hotels as $hotel) { ?>
 
-                }
-            
-            
-            
-            
-            
-            
-            ?>
-            
+                            <!-- nome -->
+                            <td>
+                                <?php echo $hotel['name']; ?>
+                            </td>
+
+                            <!-- descrizione -->
+                            <td>
+                                <?php echo $hotel['description']; ?>
+                            </td>
+
+                            <!-- parcheggio  -->
+                            <td>
+                                <?php echo $hotel['parking'] ? 'Yes' : 'No'; ?>
+                            </td>
+
+                            <!-- voto / 5 -->
+                            <td>
+                                <?php echo $hotel['vote']; ?> / 5
+                            </td>
+
+                            <!-- tot km -->
+                            <td>
+                                <?php echo $hotel['distance_to_center']; ?> km
+                            </td>
+                    </tr>
+
+                        <?php } ?>
+
+                </tbody>
+            </table>
 
         </div>
+        
+    </div>
+
+    <!-- Bootstrap script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     
 </body>
 </html>
